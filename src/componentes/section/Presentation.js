f1f38d3aea3bd6c92843222fb771d8fc45970e19
Matheus <matheus_martins@icloud.com>
2024-07-08 17:20:43 -0300

    versao-2

--- src/componentes/section/Presentation.js
+++ src/componentes/section/Presentation.js
@@ -1,14 +1,51 @@
 import styles from './Presentation.module.css'
 import ButtonA from './elements/ButtonA'
+import {useEffect, useState} from 'react'
 
 
 
 function Presentation (){
+    const [text, setText] = useState('');
+    const toRotate = ['Matheus Martins!', 'Desenvolvedor Full Stack Jr'];
+    const [loop, setLoop] = useState(0);
+    const [isDeleting, setIsDeleting] = useState(false);
+    const period = 100;
+    const [delta, setDelta] = useState(100)
+
+
+    useEffect(()=>{
+        let ticker = setInterval(()=>{
+            toType()
+        }, delta)
+        return()=> {clearInterval(ticker)}
+
+    }, [text])
+
+    const toType = () =>{
+        let i = loop % toRotate.length;
+        let fullText = toRotate[i]
+        let updatedText = isDeleting ? fullText.substring(0,text.length-1) : fullText.substring(0,text.length+1)
+
+        setText(updatedText);
+
+        if(!isDeleting && updatedText === fullText){
+            setIsDeleting(true);
+            setDelta(period);
+
+        } else if (isDeleting && updatedText === ''){
+            setIsDeleting(false);
+            setDelta(period);
+            setLoop(loop+1);
+
+        }
+    }
+
+
     return (
         <div className={styles.presentation} id='Presentation'>
            <h4><strong>Bem-vindo ao meu Portfólio</strong></h4>
-           <h1>Olá, eu sou o Matheus Martins!</h1>
-           <p>Sou um amante da tecnologia/programação e idéias inovadoras! Como desenvolvedor Full Stack,<br/>
+           <h1>Olá, eu sou {text}</h1>
+           <p>Sou um amante da tecnologia/programação e idéias inovadoras! Como desenvolvedor Full Stack Jr,<br/>
                tenho como compromisso resolver problemas complexos e trazer resultados maravilhosos para os<br/>
                projetos e negócios, sempre em busca de novos desafios! Possuo uma forte capacidade de aprendizado,<br/>
                sou organizado e tenho facilidade em trabalhar em equipe, habilidades que acredito serem essenciais<br/>
--- src/componentes/section/elements/Card.js
+++ src/componentes/section/elements/Card.js
@@ -1,19 +1,38 @@
 import styles from './Card.module.css'
-import ButtonB from './ButtonB'
+import ButtonB from './ButtonB' 
+import {useState} from 'react'
 
 function Card({img, title, tech, description, repo, site}){
+
+    const [info, setInfo] = useState(false)
+
+    function InfoOn(){
+        setInfo(true)
+    }
+
+    function InfoOff(){
+        setInfo(false)
+    }
+
+
+
+
     return (
-        <div className={styles.card}>
-           <a href={site}>
+        <div onMouseLeave={InfoOff} className={styles.card}>
+           <a onMouseEnter={InfoOn} href={site}>
                <img src={img}/>
            </a>
-           <section>
+
+           {info == true &&(
+               <section>
                <h3>{title}</h3>
                <p> <strong>Tecnologia: </strong>{tech}</p>
                <p>{description}</p>
                <ButtonB text='Acesse o repositório' link={repo}/>
 
            </section>
+           )}
+           
 
         </div>
     )
